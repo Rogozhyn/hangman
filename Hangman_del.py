8e47628446b6b93f98f03eb57d41bcd0cee830fd
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-01-05 22:30:21 +0200

    my first commit

---
+++ Hangman_del.py
@@ -0,0 +1,39 @@
+def hangman (word):
+    wrong_guesses = 0
+    stages = ["",
+              "_________",
+              "|     | ",
+              "|     0",
+              "|    /|\ ",
+              "|    / \ ",
+              "| ",
+              "---------"]
+    letters_left = list(word)
+    score_board = ['_'] * len(word)
+    win = False
+    print('Welcome to Hang Man')
+    while wrong_guesses < len(stages) - 1:
+        print(' \n ')
+        guess = input("Guess a letter: ")
+        if guess in letters_left:
+            character_index = letters_left.index(guess)
+            score_board[character_index] = guess
+            letters_left[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print(' '.join(score_board))
+        print(' \n '.join(stages[0: wrong_guesses + 1]))
+        if '_' not in score_board:
+            print('\nYou win! The word was {}\n'.format(word))
+            win = True
+            break
+    if not win:
+        print(' \n '.join(stages[0: wrong_guesses + 1]))
+        print('\nYou lose! The words was {}\n'.format(word))
+
+
+if __name__ == "__main__":
+    while True:
+        word = input("Enter word to play Hangman: ")
+        hangman(word)
+    
